93ce55d74a7d25f349e8b00f6213f8a571ce82ad
KUNWEI <kwei@thoughtworks.com>
2022-10-12 07:03:04 +0800

    feat: create person class that can introduce itself.

---
+++ OOStepByStep/Person.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+namespace OOStepByStep
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string IntroduceSelf()
+        {
+            return $"My name is {name}. I am {age} years old.";
+        }
+    }
+}
--- OOStepByStepTest/UnitTest1.cs
+++ OOStepByStepTest/UnitTest1.cs
@@ -11,5 +11,14 @@ namespace OOStepByStepTest
             var class1 = new Class1();
             Assert.NotNull(class1);
         }
+
+        [Fact]
+        public void Should_return_mesasge_when_introduce_given_a_person()
+        {
+            string name = "Tom";
+            int age = 21;
+            var person = new Person(name, age);
+            Assert.Equal("My name is Tom. I am 21 years old.", person.IntroduceSelf());
+        }
     }
 }
