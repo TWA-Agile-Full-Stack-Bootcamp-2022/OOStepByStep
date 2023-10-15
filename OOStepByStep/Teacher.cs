0d23cc7b6052041b088a6187e34e6f05a9f026e8
Jiancheng LI <jiancheng.li@thoughtworks.com>
2023-10-15 12:40:35 +0800

    feat: return teacher with class info

--- OOStepByStep/Teacher.cs
+++ OOStepByStep/Teacher.cs
@@ -1,17 +1,31 @@
-﻿using System;
+﻿using OOStepByStepTest;
+using System;
 using System.Collections.Generic;
 
 namespace OOStepByStep
 {
     public class Teacher : Person
     {
+        private Classroom classroom;
+
         public Teacher(string name, int age) : base(name, age)
         {
         }
 
+        public Teacher(string name, int age, Classroom classroom) : this(name, age)
+        {
+            this.classroom = classroom;
+        }
+
         public override void Introduce()
         {
-            Console.WriteLine($"My name is {Name}. I am {Age} years old. I am a teacher.");
+            var teacherInfo = "I am a teacher";
+            if (classroom != null)
+            {
+                teacherInfo += " of class " + classroom.Number;
+            }
+
+            Console.WriteLine($"My name is {Name}. I am {Age} years old. {teacherInfo}.");
         }
     }
 }
--- OOStepByStepTest/TeacherTest.cs
+++ OOStepByStepTest/TeacherTest.cs
@@ -25,5 +25,25 @@ namespace OOStepByStepTest
             // Then
             Assert.Equal($"My name is {givenName}. I am {givenAge} years old. I am a teacher.\r\n", fakeOutput.ToString());
         }
+
+        [Fact]
+        public void Should_return_the_teacher_with_class_information_when_introduce_given_the_name_and_age()
+        {
+            // Given
+            string givenName = "Amy";
+            int givenAge = 30;
+            int givenClassNumber = 2;
+            var classroom = new Classroom(givenClassNumber);
+            var person = new Teacher(givenName, givenAge, classroom);
+
+            var fakeOutput = new StringBuilder();
+            Console.SetOut(new StringWriter(fakeOutput));
+
+            // When
+            person.Introduce();
+
+            // Then
+            Assert.Equal($"My name is {givenName}. I am {givenAge} years old. I am a teacher of class {givenClassNumber}.\r\n", fakeOutput.ToString());
+        }
     }
 }
